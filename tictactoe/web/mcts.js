7e99bf6d0695826214a5bcee4bb703a54dba6d1b
Mark <sundryan@gmail.com>
2015-06-01 23:18:51 -0400

    Algorithm working. Need to troubleshoot animations

--- tictactoe/web/mcts.js
+++ tictactoe/web/mcts.js
@@ -1,124 +1,157 @@
-function MCTSNode(gamestate, parent, move) {
-  this.gamestate = gamestate;
-  this.parent = parent ? parent : null;
-  this.move = move ? move : null;
-  this.visits = 0;
-  this.value = 0;
-  this.children = [];
+var mcts = mcts || {};
 
-  this.frontier = this.gamestate.getLegalMoves();
-  this.frontier = shuffle(this.frontier);
-}
+(function(mcts) {
 
-MCTSNode.prototype.isFullyExpanded = function () {
-  return this.frontier.length === 0;
-};
+  function MCTSNode(gamestate, parent, move) {
+    this.gamestate = gamestate;
+    this.parent = parent ? parent : null;
+    this.move = move !== undefined ? move : null;
+    this.visits = 0;
+    this.value = 0;
+    this.children = [];
 
-MCTSNode.prototype.selectBestChild = function () {
-
-  var uctValues = this.children.map(function(node) {
-    return this.uct(node);
-  });
-
-  return max(uctValues);
-
-};
-
-MCTSNode.prototype.selectBestMove = function () {
-  var bestChild;
-
-  bestChild = max(this.children.map(function(child) {
-    return child.value / child.visits;
-  }));
-
-  return bestChild.move;
-
-};
-
-MCTSNode.prototype.isTerminal = function () {
-  return this.gamestate.winner !== null;
-};
-
-MCTSNode.prototype.expand = function () {
-  var randomMove = this.frontier.pop();
-  var newGameState = this.gamestate.copy();
-  newGameState.executeMove(randomMove);
-  var newNode = MCTSNode(newGameState, this, randomMove);
-  this.children.push(newNode);
-
-  return newNode;
-};
-
-MCTSNode.prototype.uct = function (node) {
-  var w = node.value;
-  var n = node.visits;
-  var C = Math.sqrt(2);
-  var N = this.visits;
-
-  var uct = w / n + C * Math.sqrt(Math.log(N) / n);
-
-  return uct;
-
-};
-
-function select(node) {
-  if (node.isTerminal()) {
-    return node;
-  } else if (node.isFullyExpanded()) {
-    return select(node.selectBestChild());
-  } else {
-    return node.expand();
-  }
-}
-
-function simulate(node) {
-
-  var reward;
-  var gamestate = node.gamestate.copy();
-  var currentPlayer = gamestate.currentPlayer;
-
-  while (gamestate.winner === null) {
-    var legalMoves = gamestate.getLegalMoves();
-    var move = legalMoves[Math.floor(Math.random() * legalMoves.length)];
-    gamestate.executeMove(move);
-
+    this.frontier = this.gamestate.getLegalMoves();
+    this.frontier = shuffle(this.frontier);
   }
 
-  if (gamestate.winner === 0) {
-    reward = 0;
-  } else if (gamestate.winner === 1 && currentPlayer === 1) {
-    reward = -1;
-  } else if (gamestate.winner === 2 && currentPlayer === 2) {
-    reward = -1;
-  } else {
-    reward = 1;
+  MCTSNode.prototype.isFullyExpanded = function () {
+    return this.frontier.length === 0;
+  };
+
+  MCTSNode.prototype.selectBestChild = function () {
+
+    var maxUct = this.uct(this.children[0]);
+    var maxChildIndex = 0;
+
+    for (var i = 1; i < this.children.length; i++) {
+      var child = this.children[i];
+      var uct = this.uct(child);
+
+      if (uct > maxUct) {
+        maxUct = uct;
+        maxChildIndex = i;
+      }
+
+    }
+
+    return this.children[maxChildIndex];
+
+  };
+
+  MCTSNode.prototype.selectBestMove = function () {
+
+    var maxScore = this.children[0].value / this.children[0].visits;
+    var bestChildIndex = 0;
+
+    for (var i = 1; i < this.children.length; i++) {
+      var child = this.children[i];
+      var score = child.value / child.visits;
+
+      if (score > maxScore) {
+        maxScore = score;
+        bestChildIndex = i;
+      }
+
+    }
+
+    var bestChild = this.children[bestChildIndex];
+
+    return bestChild.move;
+
+  };
+
+  MCTSNode.prototype.isTerminal = function () {
+    return this.gamestate.winner !== null;
+  };
+
+  MCTSNode.prototype.expand = function () {
+    var randomMove = this.frontier.pop();
+    if (randomMove === null) {
+      console.log('this is fucking stuid');
+    }
+    var newGameState = this.gamestate.copy();
+    newGameState.executeMove(randomMove);
+    var newNode = new MCTSNode(newGameState, this, randomMove);
+    this.children.push(newNode);
+
+    return newNode;
+  };
+
+  MCTSNode.prototype.uct = function (node) {
+    var w = node.value;
+    var n = node.visits;
+    var C = Math.sqrt(2);
+    var N = this.visits;
+
+    var uct = w / n + C * Math.sqrt(Math.log(N) / n);
+
+    return uct;
+
+  };
+
+  mcts.MCTSNode = MCTSNode;
+
+  mcts.select = function(node) {
+
+    if (node.isTerminal()) {
+      return node;
+    } else if (node.isFullyExpanded()) {
+      return mcts.select(node.selectBestChild());
+    } else {
+      return node.expand();
+    }
   }
 
-  return reward;
+  mcts.simulate = function(node) {
 
-}
+    var reward;
+    var gamestate = node.gamestate.copy();
+    var currentPlayer = gamestate.currentPlayer;
 
-function backpropagate(node, reward) {
-  node.visits += 1;
-  node.value += reward;
-  if (node.parent) {
-    backpropagate(node.parent, -reward);
+    while (gamestate.winner === null) {
+      var legalMoves = gamestate.getLegalMoves();
+      var move = legalMoves[Math.floor(Math.random() * legalMoves.length)];
+      gamestate.executeMove(move);
+
+    }
+
+    if (gamestate.winner === 0) {
+      reward = 0;
+    } else if (gamestate.winner === 1 && currentPlayer === 1) {
+      reward = -1;
+    } else if (gamestate.winner === 2 && currentPlayer === 2) {
+      reward = -1;
+    } else {
+      reward = 1;
+    }
+
+    return reward;
+
   }
 
-}
+  mcts.backpropagate = function(node, reward) {
+    node.visits += 1;
+    node.value += reward;
+    if (node.parent) {
+      mcts.backpropagate(node.parent, -reward);
+    }
 
-function mcts(root, iterations) {
-  var i = 0;
-
-  while (i < iterations) {
-    var node = select(root);
-    var reward = simulate(node);
-    backpropagate(node, reward);
-    i += 1;
   }
 
-  var bestMove = root.selectBestMove();
+  mcts.mcts = function(root, iterations) {
+    var i = 0;
 
-  return bestMove;
+    while (i < iterations) {
+      var node = mcts.select(root);
+      var reward = mcts.simulate(node);
+      mcts.backpropagate(node, reward);
+      i += 1;
+    }
 
-}
+    var bestMove = root.selectBestMove();
+
+    return bestMove;
+
+  }
+
+})(mcts);
