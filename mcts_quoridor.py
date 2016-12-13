1c133d2560b7d220e8d75d3f462840fbed5aace5
mjc <mcorriere@draper.com>
2016-12-13 17:16:52 -0500

    Rearranged project folder structure

---
+++ mcts_quoridor.py
@@ -0,0 +1,23 @@
+
+import mcts
+import quoridor
+
+def playMCTSgame():
+
+    q = quoridor.QuoridorGameState()
+    root = mcts.MCTSNode(q)
+    node = root
+
+    while q.winner is None:
+        move = mcts.mcts(node, 2000)
+        q.executeMove(move)
+        node = mcts.MCTSNode(q)
+        print q
+        print "Move:", move
+
+    print "Winner is player", q.winner
+
+if __name__ == "__main__":
+    playMCTSgame()
+
+
